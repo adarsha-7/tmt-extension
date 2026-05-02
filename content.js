e14fa22bdd0935f63c824d2d2ab251563d9b0610
Loopfelloff <abhiyanregmi@gmail.com>
2026-05-02 22:47:41 +0545

    fix : removed res.json() from content.js

--- content.js
+++ content.js
@@ -585,8 +585,6 @@ async function startTranslation(videoId, targetLang) {
     injectOverlay();
     console.log("here we are servering")
     const res = await fetch(`${BASE_URL}/api/translate/yt?videoId=${videoId}&targetedLang=${targetLang}`);
-    const pre_data = await res.json()
-    console.log(pre_data)
     if (!res.ok) throw new Error("API failed");
 
     const data = await res.json();
